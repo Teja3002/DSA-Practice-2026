5a2e668ac458b7137c02030d6a743f8ebe18beee
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-05-16 15:32:58 -0400

    Solved: If Else Adult Teen Problem (Easy) - Fundamentals Of Programming

---
+++ Fundamentals_Of_Programming/Easy/If_Else_Adult_Teen_Problem.cpp
@@ -0,0 +1,19 @@
+/*
+ * Problem: If Else Adult Teen Problem
+ * Difficulty: Easy
+ * Topic: Fundamentals Of Programming
+ * Platform: TUF+ (takeuforward.org)
+ * Date: 2026-05-16
+ */
+
+class Solution {
+public:
+    void isAdult(int age) {
+    if(age<18){
+        cout<<"Teen";
+    }
+    else{
+        cout<<"Adult";
+    }
+    }
+};
