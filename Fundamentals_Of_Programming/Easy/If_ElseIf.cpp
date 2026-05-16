843d36f1ada3afe2d03aa806ef653d7b80ea4a65
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-05-16 15:35:38 -0400

    Solved: If ElseIf (Easy) - Fundamentals Of Programming

---
+++ Fundamentals_Of_Programming/Easy/If_ElseIf.cpp
@@ -0,0 +1,21 @@
+/*
+ * Problem: If ElseIf
+ * Difficulty: Easy
+ * Topic: Fundamentals Of Programming
+ * Platform: TUF+ (takeuforward.org)
+ * Date: 2026-05-16
+ */
+
+class Solution {
+public:
+    void studentGrade(int marks) {
+if(marks>=90){
+    cout<<"Grade A";
+    }
+else if (marks>=70){
+
+cout<<"Grade B";}    
+    
+else if(marks>=50){
+    cout<<"Grade C";
+}
