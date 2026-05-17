c2d6ed59906a04367a4640b774be1a902eff88a8
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-05-17 16:36:15 -0400

    Solved: Pattern 18 (Easy) - Fundamentals Of Programming

---
+++ Fundamentals_Of_Programming/Easy/Pattern_18.cpp
@@ -0,0 +1,13 @@
+for (int i = 1; i <= n; i++) {
+
+            char start = 'A' + n - i;
+
+            for (char ch = start; ch <= 'A' + n - 1; ch++) {
+                cout << ch << " ";
+            }
+
+            cout << endl;
+        }
+    void pattern18(int n) {
+    }
+public:
