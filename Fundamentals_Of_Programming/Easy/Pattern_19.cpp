7cccdcc78554c4da86cd3e8ac33f70f67369a455
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-05-17 17:06:00 -0400

    Solved: Pattern 19 (Easy) - Fundamentals Of Programming

---
+++ Fundamentals_Of_Programming/Easy/Pattern_19.cpp
@@ -0,0 +1,12 @@
+cout << " ";
+            }
+
+            // right stars
+            for (int j = 0; j <= i; j++) {
+                cout << "*";
+            }
+
+            cout << endl;
+        }
+    }
+};
