8758cc5daf1975d87e7cee98403e100f7c0fd886
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-05-17 16:32:33 -0400

    Solved: Pattern 17 (Easy) - Fundamentals Of Programming

--- Fundamentals_Of_Programming/Easy/Pattern_17.cpp
+++ Fundamentals_Of_Programming/Easy/Pattern_17.cpp
@@ -1,13 +1,12 @@
-for( int j=1;j<=n-i;j++)
-    {
-cout<<" ";
-     }
-    for(char ch ='A';ch<='A'+i-1;ch++)
-    {
-        cout<<ch;
+cout << " ";
+            }
+
+            // characters
+            for (char ch = 'A'; ch <= 'A' + i - 1; ch++) {
+                cout << ch;
+            }
+
+            cout << endl;
+        }
     }
-    char ch;
-for(int i=1;i<=n;i++){
-    void pattern17(int n) {
-public:
-class Solution {
+};
