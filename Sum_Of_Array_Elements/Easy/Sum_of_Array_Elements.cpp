0ca2cdf1ee5309570c477938c329176d362f5812
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-05-21 19:26:13 -0400

    Solved: Sum of Array Elements (Easy) - Sum Of Array Elements

--- Sum_Of_Array_Elements/Easy/Sum_of_Array_Elements.cpp
+++ Sum_Of_Array_Elements/Easy/Sum_of_Array_Elements.cpp
@@ -1,13 +1,4 @@
-public:
-    int sum(int arr[], int n) {
-
-        int ans = 0; 
-        
-         for(int i = 0; i < n; i++) {
-             ans = ans + arr[i]; 
-        }
-        
-         return ans;
-    }
-};
-class Solution{
+class Solution:
+    def sum(self,arr, n): 
+       a= sum(arr)
+       return a
